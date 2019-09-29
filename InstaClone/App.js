a48d3c6c454b3a0486b589f77492d1053a71d4b4
João Victor Ferreira <jferreira.1997@utfpr.edu.br>
2019-09-29 15:11:22 -0300

    Add Components Posts, Comments

--- InstaClone/App.js
+++ InstaClone/App.js
@@ -1,10 +1,25 @@
-import React, {Component} from 'react';
-import Header from './src/components/Header'
+import React, { Component } from 'react';
+import Header from './src/components/Header';
+import { View, Text } from 'react-native';
+import Post from './src/components/Post';
 
 export default class App extends Component {
-  render () {
+  render() {
+    const comments = [{
+      nickname: 'Joana Elena Silva',
+      comment: 'Execelente foto!'
+    },
+    {
+      nickname: 'Joao Victor Ferreira',
+      comment: 'Da para melhorar...'
+    }];
+
     return (
-      <Header />
-    );
+      <View style={{ flex: 1 }}>
+        <Header />
+        <Post image={require('./assets/imgs/fence.jpg')}
+        comments={comments} />
+      </View>
+    )
   }
 }
