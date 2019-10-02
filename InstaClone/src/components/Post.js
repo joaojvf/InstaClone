2b7471215c8ad447e923bafdab05ef73f4c61301
João Victor Ferreira <jferreira.1997@utfpr.edu.br>
2019-10-01 22:51:35 -0300

    Criacao do componente addComentario

--- InstaClone/src/components/Post.js
+++ InstaClone/src/components/Post.js
@@ -8,6 +8,7 @@ import {
 
 import Author from './Author';
 import Comments from './Comments';
+import AddComments from './AddComments';
 
 class Post extends Component {
     render() {
@@ -16,6 +17,7 @@ class Post extends Component {
                 <Image source={this.props.image} style={styles.image} />
                 <Author email='fulano@gmail.com' nickname='Fulano de Tal' />
                 <Comments comments={this.props.comments}/>
+                <AddComments />
             </View>
         )
     }
